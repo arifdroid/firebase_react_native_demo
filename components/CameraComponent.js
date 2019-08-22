2bec8bdfd50be8d8beca69717aea0fa4af6cc7e3
arif hanif <arifdroiddeveloper@gmail.com>
2019-08-22 01:35:25 -0700

    mlkit test branched

--- components/CameraComponent.js
+++ components/CameraComponent.js
@@ -3,11 +3,47 @@ import { Text, View, TouchableOpacity } from 'react-native';
 
 import {RNCamera} from 'react-native-camera';
 
+import RNMLkit from 'react-native-firebase-mlkit';
+//import console = require('console');
+
 export default class CameraComponent extends React.Component {
+
+    constructor(){
+        super();
+
+
+    }
+
+    _takeThemPic = async()=>{
+
+        //if(this.camer)
+        if(this.camera){
+
+            const options = { quality: 0.5, base64:true, skipProcessing:true, forceOrientation:true};
+
+            const data = await this.camera.takePictureAsync(options);
+
+            const deviceTextRecognition = await RNMLkit.deviceTextRecognition(data.uri);
+
+            console.log('text detected ==>> '+ deviceTextRecognition);
+            console.log('text convert ==>> '+ JSON.stringify(deviceTextRecognition));
+            
+
+            alert('snapped');
+            
+
+        }
+
+    }
+
+
     render() {
         return (
             <View style={{ flex:1, backgroundColor:'black'}}>
                 <RNCamera
+
+                    ref={ref =>{this.camera = ref}}
+
                   style={{flex:1, justifyContent:'flex-end',alignItems:'center'}}
                   type={RNCamera.Constants.Type.front}                
                 />
@@ -18,6 +54,9 @@ export default class CameraComponent extends React.Component {
                     paddingHorizontal: 20,
                     alignSelf: 'center',
                     margin: 20,}}
+                   
+                    onPress={this._takeThemPic}
+                   
                     >
                     
                         <Text style={{fontSize:14}}>SNAP</Text>
